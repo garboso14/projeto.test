7aa3619847b9bb151b5f80154b5fb26652678da8
Iago <Iagocardosocardoso@gmail.com>
2022-10-04 15:37:30 -0300

    0

--- sucata/settings.py
+++ sucata/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'sucata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
+
+
+
 
 
 # Password validation
